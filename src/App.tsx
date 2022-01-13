f08a6ba6c775ace97154f6d46aa7fc2286ccc82f
Lindsay Jopson <lindsayjopson@gmail.com>
2022-01-13 15:31:08 +1300

    adding ALL THE THINGS

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import VegasBg from './assets/images/vegas.png';
+import MovioLogo from './assets/images/movio.svg';
+import TimerComp from './components/TimerComp';
+
+function App() {
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-full">
+          <div
+            className="h-full"
+            >
+            <div className="py-8 h-full flex flex-col justify-between">
+              <img className="flex flex-col items-center m-auto w-32 mt-6" src={MovioLogo} />
+              <div className="flex flex-col items-center absolute top-1/3 w-full">
+                <span className="text-white font-bold text-lg tracking-3xl text-center">
+                  CINEMACON LAUNCH
+                </span>
+                <img className="flex flex-col items-center m-auto fixed bottom-0" style={{width: '900px'}} src={VegasBg} />
+                <div>
+                  <TimerComp />
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,75 @@
+import {
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
+
+import {
+  setDate,
+  setHours,
+  setMinutes,
+  setSeconds,
+  differenceInSeconds,
+} from 'date-fns';
+
+const baseDate = +new Date(`04/25/2022`);
+
+const useTimer = () => {
+  const timerRef = useRef(0);
+  const intervalRef = useRef<any>();
+
+  const [day, setDay] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  const updateTimeValues = useCallback((difference: number) => {
+    var d = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var h = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    var m = Math.floor((difference / 1000 / 60) % 60);
+    var s = Math.floor((difference / 1000) % 60);
+
+    setDay(d);
+    setHour(h);
+    setMinute(m);
+    setSecond(s);
+  }, []);
+
+  const calculateEndTime = useCallback(() => {
+    let difference = baseDate - +new Date();
+
+    timerRef.current = difference;
+  
+  }, []);
+
+  const run = useCallback(() => {
+    decreaseNum();
+    intervalRef.current = setInterval(decreaseNum, 1000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const decreaseNum = useCallback(() => {
+    if (timerRef.current >= 0) {
+      updateTimeValues(baseDate - +new Date());
+      timerRef.current = timerRef.current - 1;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    calculateEndTime();
+    run();
+    return () => clearInterval(intervalRef.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    day,
+    hour,
+    minute,
+    second,
+  };
+};
+
+export default useTimer;
