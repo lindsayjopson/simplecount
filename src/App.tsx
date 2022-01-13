20c8c192dce9bee05d3a687542e5a0b7ea9ad71b
Lindsay Jopson <lindsayjopson@gmail.com>
2022-01-14 08:57:31 +1300

    font styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,19 @@ function App() {
   return (
     <div className="h-screen bg-black">
       <div className="h-full">
-          <div
-            className="h-full"
-            >
+          <div className="h-full">
             <div className="py-8 h-full flex flex-col justify-between">
               <img className="flex flex-col items-center m-auto w-32 mt-6" alt="movio logo" src={MovioLogo} />
               <div className="flex flex-col items-center absolute top-1/3 w-full">
-                <span className="text-white font-bold text-lg tracking-3xl text-center">
-                  CINEMACON LAUNCH
-                </span>
-                <img className="flex flex-col items-center m-auto fixed bottom-0" alt="las vegas background" style={{width: '900px'}} src={VegasBg} />
+                  <div className="text-white font-bold text-lg tracking-3xl text-center mb-16 text-2xl">
+                    CINEMACON LAUNCH
+                  </div>
                 <div>
                   <TimerComp />
                 </div>
+                <img className="flex flex-col items-center m-auto fixed bottom-0 opacity-20" alt="las vegas background" style={{width: '900px'}} src={VegasBg} />
               </div>
             </div>
-
           </div>
         </div>
     </div>
--- src/components/TimerBoxComp.tsx
+++ src/components/TimerBoxComp.tsx
@@ -8,7 +8,7 @@ interface ComponentProps {
 function TimerBoxComp(props: ComponentProps) {
   return (
     <div className="">
-      <div className="relative h-36 w-36 bg-darkest-blue rounded-lg">
+      <div className="relative h-36 w-52 bg-darkest-blue rounded-lg">
         <div className="absolute top-0 left-0 right-0 bottom-2 rounded-lg">
           <div className="h-full flex flex-col justify-between space-y-px">
             <div className="relative h-full w-full bg-dark-blue opacity-75 rounded-tl-lg rounded-tr-lg card__top"></div>
@@ -17,14 +17,14 @@ function TimerBoxComp(props: ComponentProps) {
         </div>
 
         <div className="absolute top-0 left-0 right-0 bottom-2 flex items-center justify-center">
-          <span className="text-white font-bold text-6xl">
+          <span className="text-white font-bold text-8xl">
             { props.value }
           </span>
         </div>
       </div>
 
       <div className="text-center">
-        <span className="uppercase text-sm text-white font-bold tracking-4xl">
+        <span className="uppercase text-sm text-white font-bold tracking-5xl">
           { props.label }
         </span>
       </div>
